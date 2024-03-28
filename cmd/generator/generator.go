566ac61a56222132bb0ba6ee873080389f9f33d3
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-27 20:50:40 -0500

    Fixing the version to v1.1.1.

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -68,7 +68,7 @@ module my-project
 // You might want to change the golang version.
 go 1.22
 
-require github.com/AlephVault/golang-standard-http-mongodb-storage v1.1.0
+require github.com/AlephVault/golang-standard-http-mongodb-storage v1.1.1
 `)
 
 var dockerFileContents = strings.TrimSpace(`
