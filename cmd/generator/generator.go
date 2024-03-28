ca790a9dc148423dad70e4b2acc8200a1b57ecbb
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-27 19:30:45 -0500

    Bumping to require v1.1.0 in the generated go.mod files.

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -68,7 +68,7 @@ module my-project
 // You might want to change the golang version.
 go 1.22
 
-require github.com/AlephVault/golang-standard-http-mongodb-storage v1.0.0
+require github.com/AlephVault/golang-standard-http-mongodb-storage v1.1.0
 `)
 
 var dockerFileContents = strings.TrimSpace(`
